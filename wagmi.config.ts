026448e9d17fa192124b0f06a8e77ff42aa63738
Daniel Tao <danieltaox@gmail.com>
2023-01-31 17:53:32 -0500

    chore: remove generated TS files from repo

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -6,13 +6,14 @@ export default defineConfig({
   out: 'src/generated.ts',
   plugins: [
     foundry({
-      deployments: {
-        Counter: {
-          [chains.mainnet.id]: '0x1A61839Eb5fC6eBBcAe01eD5E79062E598792Dac',
-          [chains.goerli.id]: '0x78991BB1D194C1235fe285240af8489CFA552151',
-          [chains.foundry.id]: '0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167',
-        },
-      },
+      // deployments: {
+      //   Deployer: {
+      //     // [chains.mainnet.id]: '0x1A61839Eb5fC6eBBcAe01eD5E79062E598792Dac',
+      //     // [chains.goerli.id]: '0x78991BB1D194C1235fe285240af8489CFA552151',
+      //     [chains.foundry.id]: '0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167',
+      //   },
+      // },
+      artifacts: '/out', // TODO: comment this out when wagmi-cli bug is fixed
       project: './contracts',
     }),
     react(),
