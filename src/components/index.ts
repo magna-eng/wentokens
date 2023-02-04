e9a549644cb449e277787130083fd7d28a4d4ef4
Daniel Tao <danieltaox@gmail.com>
2023-02-04 12:28:40 -0500

    chore: kill unused boilerplate components

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Account } from './Account';
-export { Connect } from './Connect';
-export { NetworkSwitcher } from './NetworkSwitcher';
-export { ERC20 } from './AirdropERC20';
-export { ETH } from './AirdropETH';
