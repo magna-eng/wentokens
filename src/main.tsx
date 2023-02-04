5683df3970267c81c87dadaf7e413b244ab12887
Daniel Tao <danieltaox@gmail.com>
2023-02-04 12:30:28 -0500

    feat: connect wagmi to rainbowkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import * as ReactDOM from 'react-dom/client';
 import { WagmiConfig } from 'wagmi';
 import { App } from './App';
-import { client } from './wagmi';
+import { client, chains } from './wagmi';
+
+import '@rainbow-me/rainbowkit/styles.css';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiConfig client={client}>
-      <App />
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <App />
+      </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>,
 );
