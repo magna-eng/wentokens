3e740b371f2eb48ba5b38aed0ce098e409929fcb
leggio <harrison@legg.io>
2023-03-04 10:26:42 -0700

    [COM-991] feat: Deploy Script

--- contracts/src/Airdrop.sol
+++ contracts/src/Airdrop.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 /**
  ▄█     █▄     ▄████████ ███▄▄▄▄       ███      ▄██████▄     ▄█   ▄█▄    ▄████████ ███▄▄▄▄      ▄████████ 
