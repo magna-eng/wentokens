a726eeeca4140a0aef96bf6e7780a923983df010
leggio <harrison@legg.io>
2023-03-07 08:39:59 -0500

    fix: move etherscan link to navbar

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,7 +1,17 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { tw } from 'typewind';
+import { useNetwork } from 'wagmi';
+import EtherscanLogo from '../assets/etherscan-logo.svg';
+import { airdropAddress } from '../generated';
+
+type ChainID = keyof typeof airdropAddress;
 
 export default function Navbar() {
+  const { chain } = useNetwork();
+  const chainID = (chain?.id ?? 1) as ChainID;
+  const chainName = chain?.name?.toLowerCase() ?? 'ethereum';
+  const subdomain = !chain || chainName === 'ethereum' ? '' : `${chain.name}.`;
+  const etherscanURL = `https://${subdomain}etherscan.io/address/${airdropAddress[chainID]}`;
   return (
     <div className={tw.navbar.bg_transparent.border_b_['1px'].py_4.px_4.sm(tw.px_40)}>
       <div className={tw.navbar_start}>
