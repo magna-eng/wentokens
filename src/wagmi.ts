0e00b7974bfb5a37c6dadeb84c4a0089723d3688
leggio <harrison@legg.io>
2023-03-08 12:10:25 -0500

    Feat: Add Arbitrum Main/Testnet

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,10 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient } from 'wagmi';
-import { foundry, goerli, sepolia, mainnet } from 'wagmi/chains';
+import { foundry, goerli, sepolia, mainnet, arbitrum, arbitrumGoerli } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [sepolia, mainnet, goerli, ...(import.meta.env?.MODE === 'development' ? [goerli, foundry] : [])],
+  [sepolia, mainnet, goerli, arbitrum, arbitrumGoerli, ...(import.meta.env?.MODE === 'development' ? [goerli, foundry] : [])],
   [publicProvider()],
 );
 
